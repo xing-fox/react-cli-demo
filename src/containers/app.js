16edd87745d66a6d1fc635d7287d2b7cc6071446
方醒 <450499752@qq.com>
2019-08-01 10:32:44 +0800

    first commit

---
+++ src/containers/app.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import '../style/containers/app.styl';
+
+class App extends Component {
+  state = {};
+  componentWillMount() {}
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="title">投保须知</div>
+      </div>
+    );
+  }
+}
+
+export default App;
